8a0e7455fbf8ca4f2cba6213eea294ffd4426eff
rbiedrawa <biedrawa.rafal@gmail.com>
2022-04-04 08:33:15 +0200

    feat(build): initial commit

---
+++ src/main/java/com/rbiedrawa/app/SpringQuartzFlywayStarterApplication.java
@@ -0,0 +1,13 @@
+package com.rbiedrawa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuartzFlywayStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuartzFlywayStarterApplication.class, args);
+	}
+
+}
